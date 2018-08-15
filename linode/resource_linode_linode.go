f014689bf8d284535dfdee5a8bce5684048dec7c
ChangZhuo Chen (陳昌倬) <czchen@czchen.org>
2018-08-15 14:37:28 +0800

    Add ipv4_public, ipv4_private

--- linode/resource_linode_linode.go
+++ linode/resource_linode_linode.go
@@ -4,11 +4,37 @@ import (
 	"encoding/json"
 	"fmt"
 	"log"
+	"net"
 	"time"
 
 	"github.com/hashicorp/terraform/helper/schema"
 	"github.com/sethvargo/go-password/password"
 )
+
+var privateIPBlocks []*net.IPNet
+
+func init() {
+	for _, cidr := range []string{
+		"127.0.0.0/8",    // IPv4 loopback
+		"10.0.0.0/8",     // RFC1918
+		"172.16.0.0/12",  // RFC1918
+		"192.168.0.0/16", // RFC1918
+		"::1/128",        // IPv6 loopback
+		"fe80::/10",      // IPv6 link-local
+	} {
+		_, block, _ := net.ParseCIDR(cidr)
+		privateIPBlocks = append(privateIPBlocks, block)
+	}
+}
+
+func isPrivateIP(ip net.IP) bool {
+	for _, block := range privateIPBlocks {
+		if block.Contains(ip) {
+			return true
+		}
+	}
+	return false
+}
 
 func resourceLinodeLinode() *schema.Resource {
 	return &schema.Resource{
@@ -49,6 +75,14 @@ func resourceLinodeLinode() *schema.Resource {
 				Elem: &schema.Schema{
 					Type: schema.TypeString,
 				},
+				Computed: true,
+			},
+			"ipv4_public": &schema.Schema{
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"ipv4_private": &schema.Schema{
+				Type:     schema.TypeString,
 				Computed: true,
 			},
 			"ipv6": &schema.Schema{
